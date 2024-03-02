dfdb27b8230c80a531616459c4ec85b7a709a787
devRezaulKarim <dev.rezaulkarim@gmail.com>
2024-03-02 21:25:57 +0600

    REFACTOR: Just increase the width of header filtering section on 2xl screen

--- src/components/HeaderFilters.js
+++ src/components/HeaderFilters.js
@@ -2,7 +2,7 @@ import IconsFamilies from "./IconsFamilies";
 
 const HeaderFilters = () => {
   return (
-    <div className="container max-w-[1080px] mx-auto">
+    <div className="container lg:max-w-[1080px] 2xl:xl:max-w-[1440px] mx-auto">
       <IconsFamilies />
     </div>
   );
