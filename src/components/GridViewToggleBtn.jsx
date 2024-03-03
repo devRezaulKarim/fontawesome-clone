0e1ef3cf532fcafc3b0f8dc74ed7b2d14bfd7c5e
devRezaulKarim <dev.rezaulkarim@gmail.com>
2024-03-03 08:53:07 +0600

    FEAT: Add grid view toggle buttons in header filter section
    Added grid view toggler buttons "Roomy", "Compact", "Cheatsheet" in filter section of header.

--- src/components/GridViewToggleBtn.jsx
+++ src/components/GridViewToggleBtn.jsx
@@ -1,7 +1,15 @@
-const GridViewToggleBtn = () => {
+/* eslint-disable react/prop-types */
+const GridViewToggleBtn = ({ btn }) => {
   return (
-    <div>GridViewToggleBtn</div>
-  )
-}
+    <button className="p-[1px] border-[3px] border-white focus:border-[var(--color-tertiary)] rounded-lg relative group">
+      <div className="text-xl text-[var(--color-primary)] active:bg-[var(--color-primary)] active:text-white py-0 px-3 rounded hover:text-[var(--color-secondary)]">
+        <i className={btn.icon}></i>
+        <span className="absolute left-1/2 -translate-x-1/2 -top-10 text-sm bg-[var(--color-primary)] px-3 py-1 rounded-sm after:content-[''] after:w-[10px] after:aspect-square after:bg-[var(--color-primary)] after:absolute text-white after:rotate-45 after:left-1/2 after:-translate-x-1/2 after:bottom-0 after:translate-y-1/2 after:-z-10 scale-0 group-hover:scale-100 duration-100">
+          {btn.title}
+        </span>
+      </div>
+    </button>
+  );
+};
 
-export default GridViewToggleBtn
+export default GridViewToggleBtn;
--- src/components/HeaderFilters.jsx
+++ src/components/HeaderFilters.jsx
@@ -3,7 +3,7 @@ import IconsFamilies from "./IconsFamilies";
 
 const HeaderFilters = () => {
   return (
-    <div className="container lg:max-w-[1080px] 2xl:xl:max-w-[1440px] mx-auto">
+    <div className="container lg:max-w-[1080px] 2xl:xl:max-w-[1440px] mx-auto flex items-center justify-between">
       <IconsFamilies />
 
       <GridViewToggleBtns />
